27c7326a0816400617e690e4e3e8701505545619
Vinicius Mellegari <vini.a.mellegari@gmail.com>
2024-10-29 11:01:18 -0300

    inserindo paginaçao

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 
+import debug_toolbar
 from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
+    path('__debug__/', include(debug_toolbar.urls)),
     path("admin/", admin.site.urls),
-    re_path('bookstone/(?P<version>(v1|v2))/', include('order.urls')),
-    re_path('bookstone/(?P<version>(v1|v2))/', include('product.urls'))
+    re_path('bookstore/(?P<version>(v1|v2))/', include('order.urls')),
+    re_path('bookstore/(?P<version>(v1|v2))/', include('product.urls'))
 ]
