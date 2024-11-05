48e091a5c5935eb9236211f025b1ee5d1e743a58
Vinicius Mellegari <vini.a.mellegari@gmail.com>
2024-11-05 11:06:42 -0300

    retirando a autenticação do products

--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -3,7 +3,6 @@ import json
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
-from rest_framework.authtoken.models import Token
 
 from product.factories import CategoryFactory, ProductFactory
 from order.factories import UserFactory
@@ -15,8 +14,6 @@ class TestProductViewSet(APITestCase):
 
     def setUp(self):
         self.user = UserFactory()
-        token = Token.objects.create(user=self.user)
-        token.save()
 
         self.product = ProductFactory(
             title='ppro controller',
@@ -24,8 +21,6 @@ class TestProductViewSet(APITestCase):
         )
 
     def test_get_all_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         response = self.client.get(
             reverse('product-list', kwargs={'version': 'v1'})
         )
@@ -38,8 +33,6 @@ class TestProductViewSet(APITestCase):
         self.assertEqual(product_data['results'][0]['active'], self.product.active)
 
     def test_create_product(self):
-        token = Token.objects.get(user__username=self.user.username)
-        self.client.credentials(HTTP_AUTHORIZATION='Token ' + token.key)
         category = CategoryFactory()
         data = json.dumps({
             'title': 'notebook',
